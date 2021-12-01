87019980318c4a99f289fc33e5d5a561c30f1cf2
localDev1 <fproblero85@gmail.com>
2021-12-01 14:43:26 -0800

    CALLING METHODS SUBMISSION ASSIGNMENT

---
+++ CallingMethods/CallingMethods/MathOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethods
+{
+    public class MathOperations
+    {
+        //value squared of a number
+        public static int SquareNumber (int number) 
+        {
+            return number *= number;
+        }
+
+        //value  Double of a number
+        public static int DoubleNumber (int number) 
+        {
+            return number *= 2;
+        }
+
+        //Value by Ten of a number
+        public static int ByTen (int number)
+        {
+            return number *= 10;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asking the user what number want to do the math operations on
+            Console.WriteLine("\n\n\tEnter a Integer number want to do a math operations");
+
+            //Converting string to int
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Calling the Method ByTen in the Class MathOperations
+            Console.WriteLine("\n\tTen times the number " + userNumber + " is " + MathOperations.ByTen(userNumber));
+
+            //Calling the Method ByTen in the Class MathOperations
+            Console.WriteLine("\n\tSquare the number " + userNumber + " is " + MathOperations.SquareNumber(userNumber));
+
+            //Calling the Method ByTen in the Class MathOperations
+            Console.WriteLine("\n\tDouble the number " + userNumber + " is " + MathOperations.DoubleNumber(userNumber));
+
+            Console.ReadLine();
+
+        }
+    }
+}
