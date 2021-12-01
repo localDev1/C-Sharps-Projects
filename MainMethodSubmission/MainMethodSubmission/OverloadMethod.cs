52702e5c3848cffb70f5b3eeb67c139f4d271c82
localDev1 <fproblero85@gmail.com>
2021-12-01 15:46:44 -0800

    MAIN METHOD SUBMISSION ASSIGNMENT

---
+++ MainMethodSubmission/MainMethodSubmission/OverloadMethod.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodSubmission
+{
+    class OverloadMethod
+    {
+        //Return value square of a number
+        public int ConvertMethod(int number) 
+        {
+            
+            return number *= number;
+        }
+
+        //Converting a decimal number to Integer to get the double value
+        public int ConvertMethod(decimal number) 
+        {
+            int numberConvert = (Convert.ToInt32(number))*2;
+            
+            return numberConvert;
+        }
+
+        //Converting a string to Integer to get the square value
+        public int ConvertMethod(string str, out string message)
+        {
+            message = "";
+            int numberConvert = 0;
+            //trying to convert a string in integer
+            try
+            {
+                numberConvert = (Convert.ToInt32(str)) * 10;
+            }
+            catch (Exception e)             
+            {
+                message = e.Message;
+            }            
+
+            return numberConvert;
+        }
+
+
+
+
+    }
+}
---
+++ MainMethodSubmission/MainMethodSubmission/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MainMethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class and call the one method
+            OverloadMethod methods = new OverloadMethod();
+            
+            //Using the first case (integer to integer)
+            int result = methods.ConvertMethod(7);
+
+            //Display the result to the screen.
+            Console.WriteLine("\n\n\tThe result passing a integer is (square): " + result);
+
+
+            //Using the second case (decimal to integer)
+            result = methods.ConvertMethod(7.77m);
+
+            //Display the result to the screen.
+            Console.WriteLine("\n\tThe result passing a decimal is (double): " + result);
+
+
+            //Using the third case (string to integer)
+            string message = "";
+            //result = methods.ConvertMethod("15",out message);
+            result = methods.ConvertMethod("fer", out message);
+            if (message == "")
+            {
+                //Display the result to the screen.
+                Console.WriteLine("\n\tThe result passing a string is (by Ten): " + result);
+            }
+            else 
+            {
+                Console.WriteLine("\n\tIt was not possible to convert the string to an integer");
+            }
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
