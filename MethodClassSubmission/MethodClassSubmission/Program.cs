d3e1adf7ee5933a6e54c5d465a41c07bf4e97abe
localDev1 <fproblero85@gmail.com>
2021-12-01 22:08:44 -0800

    METHOD CLASS SUBMISSION ASSIGNMENT

---
+++ MethodClassSubmission/MethodClassSubmission/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MethodClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class Rara.
+            Rara method = new Rara();
+
+            //Call the method in the class, passing in two numbers.
+            method.raraMethod(10, 15);
+
+            //Call the method in the class, specifying the parameters by name.
+            method.raraMethod(x: 15, y: 200);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClassSubmission/MethodClassSubmission/Rara.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClassSubmission
+{
+    public class Rara
+    {
+
+        //creating a void method that takes two integers as parameters
+        public void raraMethod(int x, int y)//,out int print) 
+        {
+            //the method do a math operation on the first integer
+            int f = x * x;
+            //print = y;
+            //display the second integer to the screen.
+            Console.WriteLine("\n\n\tThe second number is " + y);
+        }
+        
+       
+
+    }
+}
