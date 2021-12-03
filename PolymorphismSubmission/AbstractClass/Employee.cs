73d417da0f5d46e0e1f62a2546f1e27a4b2d7687
localDev1 <fproblero85@gmail.com>
2021-12-03 12:18:56 -0800

    POLYMORPHISM SUBMISSION ASSIGNMENT

---
+++ PolymorphismSubmission/AbstractClass/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    public class Employee:Person,IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("\n\n\t The name is :" + fisrtName +" "+ lastName);
+        }
+
+        //Implementig the method inside the Iquattable Interface
+        public void Quit() 
+        {
+            //Change the name 
+            fisrtName = "Fernando";
+            lastName = "Perez Roblero";
+            Console.WriteLine("\n\n\t The new name is :" + fisrtName + " " + lastName);
+        }
+    }
+}
---
+++ PolymorphismSubmission/AbstractClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an object of type IQuittable using Polymorphism
+            IQuittable employee = new Employee();
+
+           //Call the Quit() method on the object employee.
+            employee.Quit();
+
+            Console.ReadLine();
+
+        }
+    }
+}
