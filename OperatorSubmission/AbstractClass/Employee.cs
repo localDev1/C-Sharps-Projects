0d05bd9b9d64cdcda780e0aed55dd3eab67881e6
localDev1 <fproblero85@gmail.com>
2021-12-03 15:43:11 -0800

    OPERATORS SUBMISSION ASSIGNMENT

---
+++ OperatorSubmission/AbstractClass/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorsSubmission
+{
+    public class Employee:Person,IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("\n\n\t The name is :" + fisrtName +" "+ lastName);
+        }
+
+        //Implementig the method inside the Iquattable Interface
+        public void Quit() 
+        {
+            //Change the name 
+            fisrtName = "Fernando";
+            lastName = "Perez Roblero";
+            Console.WriteLine("\n\n\t The new name is :" + fisrtName + " " + lastName);
+        }
+
+        //Overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property
+        public static string operator ==(Employee person1, Employee person2) 
+        {
+            if (person1.Id == person2.Id)
+                return "Yes, are equal!";
+            else
+                return "No, are not equal!";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public static string operator !=(Employee person1, Employee person2)
+        {
+            if (person1.Id != person2.Id)
+                return "Are NOT equal!";
+            else
+                return "Are equal!";
+        }
+
+
+    }
+}
---
+++ OperatorSubmission/AbstractClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OperatorsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Creating two objects Employee
+            Employee person1 = new Employee() { Id = 10 };
+            
+            Employee person2 = new Employee() { Id = 10 };
+            
+
+
+            //Using the overload "==" operator  
+            string igual = person1 == person2;
+
+            Console.WriteLine("\n\n\t"+ igual);
+
+            Console.ReadLine();
+
+        }
+    }
+}
