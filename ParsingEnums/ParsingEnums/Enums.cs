0a20ac097c60dbb31ba548deca256d9c3f5456b8
localDev1 <fproblero85@gmail.com>
2021-12-06 11:32:40 -0800

    PARSING ENUMS SUBMISSION ASSIGNMENT

---
+++ ParsingEnums/ParsingEnums/Enums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParsingEnums
+{
+    class Enums
+    {
+    }
+
+    //Enum for the days of the week.
+    public enum DaysOfWeek 
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompt the user to enter the numbber current day of the week.
+            Console.WriteLine("\n\n\tEnter the number current day of the week");
+
+
+            try
+            {
+                //Assign the value to a variable of that enum data type you just created.
+                int dayUser = Convert.ToInt32(Console.ReadLine());
+
+                //Parsing the integer to enum DaysOfWeek
+                DaysOfWeek day = (DaysOfWeek)dayUser;
+                Console.Write("The current Day is :" + day);
+
+            }
+            catch 
+            {
+                //if an error occurs to converting 
+                Console.Write("Please enter an actual day of the week");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
