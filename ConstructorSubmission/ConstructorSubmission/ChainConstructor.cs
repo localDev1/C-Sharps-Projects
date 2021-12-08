641c5037c56b67e6ff85be1ef60e567d5e2d4642
localDev1 <fproblero85@gmail.com>
2021-12-08 15:16:34 -0800

    CONSTRUCTOR SUBMISSION ASSIGNMENT

---
+++ ConstructorSubmission/ConstructorSubmission/ChainConstructor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorSubmission
+{
+    public class ChainConstructor
+    {
+        //Chain two constructors together.
+        public ChainConstructor():this("pendiente", "pendiente") 
+        {
+        }
+
+        public ChainConstructor(string fname, string lname) 
+        {
+            firstName = fname;
+            lastName = lname;
+        }
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        
+    }
+}
---
+++ ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConstructorSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a const variable.
+            const string wel = "Hello everyone!";           
+
+            Console.WriteLine("\n\n\t"+wel);
+
+            Console.WriteLine("\n\n\tEnter your First name:");
+            //Using the keyword "var"
+            var fname = "";
+            fname = Console.ReadLine();
+           
+            Console.WriteLine("\n\n\tEnter your Last Name");
+            //Using the keyword "var"
+            var lname = "";
+            lname = Console.ReadLine();
+
+
+            ChainConstructor chain = new ChainConstructor();
+            Console.WriteLine("\n\n\t Using the first Constructor the result is :" + chain.firstName + " " + chain.lastName);
+
+            ChainConstructor chain1 = new ChainConstructor(fname, lname);
+            Console.WriteLine("\n\n\t Using the Second Constructor the result is :" + chain1.firstName + " " + chain1.lastName);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
