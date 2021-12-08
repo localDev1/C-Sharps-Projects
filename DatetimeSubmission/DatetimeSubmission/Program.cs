557e1409d40310a1e65d06c461fa39a4fd815916
localDev1 <fproblero85@gmail.com>
2021-12-08 11:32:05 -0800

    DATE/TIME SUBMISSION ASSIGNMENT

---
+++ DatetimeSubmission/DatetimeSubmission/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DatetimeSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Printing the current date and time to the console.
+            Console.WriteLine("\n\n\tHello World!, We are in the day : " + DateTime.Now);
+
+            //Asking the user a number
+            Console.WriteLine("\n\n\tEnter a number less 24");
+
+            //Converting string to int
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            if (x < 24) 
+            {
+                //Converting int to datatime 
+                DateTime hours = new DateTime(1975, 3, 23, x, 59, 59);
+
+                Console.WriteLine("\n\n\tThe user hour is : " + hours.Hour);
+            }
+
+            else 
+            {
+                Console.WriteLine("you dont introduce a number less 24");
+            }
+            Console.ReadLine();
+        }
+    }
+}
